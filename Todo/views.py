bce1ea1569e3f0e58eae3a76ca68b2105c6b6d19
Alireza Najafi <Alireza.Nj.Rose@gmial.com>
2023-01-17 21:37:19 +0330

    'submit & delete btn style '

--- Todo/views.py
+++ Todo/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render , redirect
-from django.http import HttpResponse
 from .models import TodoModel
 # Create your views here.
 def change(request,id):
-    if request.method =="GET":
+    if request.method == "GET":
         work = TodoModel.objects.get(id=id)
         done = request.GET.get('done')
         if done == 'on':
             work.done = True
             work.save()
         return redirect('Home')
+    else:
+        pass
        
